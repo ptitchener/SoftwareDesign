3a8247986a5439f05b0426e3fa90cf6e06164424
Paul Titchener <paul.titchener@students.olin.edu>
2014-02-13 14:09:51 -0500

    updates to gene_finder (added author)

--- hw3/gene_finder.py
+++ hw3/gene_finder.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Feb  2 11:24:42 2014
 
-@author: YOUR NAME HERE
+@author: Paul
 """
 
 # you may find it useful to import these variables (although you are not required to use them)
